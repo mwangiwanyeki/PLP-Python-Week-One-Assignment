cc498072c386f1f6441f76b607beb32ce115bac1
mwangiwanyeki <37798128+mwangiwanyeki@users.noreply.github.com>
2024-02-28 14:51:44 +0300

    First python commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your current location: ")
+
+print("Hello", name, "you are age", age, "years old and live in", location)
